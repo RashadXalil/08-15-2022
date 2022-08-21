60f849835bc95ab1990d5cc5c04dd90460a22060
RashadXalil <rashadmkh@code.edu.az>
2022-08-21 15:23:06 +0400

    backend__started

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,14 @@
-const express = require("express")
-console.log(express);
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "hbs");
+app.use(express.json());
+app.use("/public", express.static(path.resolve("assets")));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(8080, () => {
+  console.log("Server running on 8080");
+});
